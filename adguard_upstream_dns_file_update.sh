ed4bbac34af633af52cba83228fe970d3b1f1a5c
kmzs123 <kmzs1023@qq.com>
2025-09-28 02:42:01 +0800

    更新 2025-09-28 02:42:01

--- adguard_upstream_dns_file_update.sh
+++ adguard_upstream_dns_file_update.sh
@@ -2,30 +2,10 @@
 
 LANGUAGE=zh_CN:zh
 LANG=zh_CN.UTF-8
-repo_dir=/root/geosite.git
+repo_dir=$(dirname "$(readlink -f "$0")")
+https_proxy="http://127.0.0.1:10808"
 geosite_dir="$repo_dir"/geosite
-
-cd "$repo_dir" || exit 1
-
-#-e https_proxy=127.0.0.1:10808 
-#https://ghfast.top/
-if ! wget -N -e https_proxy=127.0.0.1:10808 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat ; then
-    echo "错误：下载规则文件失败！"
-    rm -f "$repo_dir"/geosite.dat
-    exit 1
-fi
-
-#-e https_proxy=127.0.0.1:10808 
-#https://gh.cytus.tk/
-if ! wget -N -e https_proxy=127.0.0.1:10808 https://github.com/MetaCubeX/geo/releases/latest/download/geo-linux-amd64 ; then
-    echo "错误：下载geo解包工具失败！"
-    rm -f "$repo_dir"/geo-linux-amd64
-    exit 1
-fi
-chmod +x "$repo_dir"/geo-linux-amd64
-
-rm -rf "$geosite_dir"
-"$repo_dir"/geo-linux-amd64 unpack site "$repo_dir"/geosite.dat -d "$geosite_dir"
+target_file=/root/Docker-compose/AdGuardHome/conf
 
 # 优化后的文件转换函数
 convert_files() {
@@ -118,7 +98,105 @@ convert_files() {
       "$geosite_file" > "$adguard_home_file"
 }
 
-. "$repo_dir"/config.sh
+cd "$repo_dir" || exit 1
+
+# https://ghfast.top/
+if ! wget -N -e https_proxy="$https_proxy" https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat ; then
+    echo "错误：下载规则文件失败！"
+    rm -f "$repo_dir"/geosite.dat
+    exit 1
+fi
+
+if ! wget -N -e https_proxy="$https_proxy" https://github.com/MetaCubeX/geo/releases/latest/download/geo-linux-amd64 ; then
+    echo "错误：下载geo解包工具失败！"
+    rm -f "$repo_dir"/geo-linux-amd64
+    exit 1
+fi
+chmod +x "$repo_dir"/geo-linux-amd64
+
+rm -rf "$geosite_dir"
+"$repo_dir"/geo-linux-amd64 unpack site "$repo_dir"/geosite.dat -d "$geosite_dir"
+
+# 建议与 openwrt网页-->网络-->DHCP/DNS-->常规-->本地域名 相同
+# 一行一个，不能带端口
+cat > "$repo_dir"/private << 'EOF'
+mir3g70
+rm2100dd
+EOF
+# 内网自定义域，可选取消注释下一行启用geosite:private
+#awk 1 "$geosite_dir"/private | awk '!seen[$0]++' >> "$repo_dir"/private
+
+# 与proxy可能会冲突并希望由国内dns解析的域名，一行一个，不能带端口
+cat > "$repo_dir"/direct1 << 'EOF'
+360.cn
+alidns.com
+doh.pub
+dot.pub
+onedns.net
+EOF
+# geosite_dir里的文件没有结尾空行，需要使用awk 1来合并，不能使用cat
+# 注意行尾的斜杠和竖线 \		| \
+# 参考 v2ray/xray/singbox的路由配置，将geosite是修改配置改一下之后写到对应位置
+awk 1 "$geosite_dir"/apple-cn \
+      "$geosite_dir"/google-cn | \
+awk '!seen[$0]++' >> "$repo_dir"/direct1 #去除重复
+
+# 国外dns黑名单
+# 一行一个，不能带端口
+cat > "$repo_dir"/proxy << 'EOF'
+1.ip.skk.moe
+EOF
+# geosite_dir里的文件没有结尾空行，需要使用awk 1来合并，不能使用cat
+# 注意行尾的斜杠和竖线 \		| \
+# 参考 v2ray/xray/singbox的路由配置，将geosite是修改配置改一下之后写到对应位置
+awk 1 "$geosite_dir"/gfw \
+      "$geosite_dir"/google \
+      "$geosite_dir"/greatfire | \
+awk '!seen[$0]++' >> "$repo_dir"/proxy #去除重复
+
+# 国内dns白名单
+# 一行一个，不能带端口
+cat > "$repo_dir"/direct2 << 'EOF'
+2.ip.skk.moe
+cytus.tk
+deepseek.com
+kmzs123.cf
+kmzs123.cn
+kmzs123.tk
+kmzs123.top
+ping0.cc
+vmshell.com
+EOF
+grep -i -h "@cn" "$geosite_dir"/category-games > "$geosite_dir"/category-games@cn
+grep -i -h "@cn" "$geosite_dir"/* > "$geosite_dir"/@cn
+# geosite_dir里的文件没有结尾空行，需要使用awk 1来合并，不能使用cat
+# 注意行尾的斜杠和竖线 \		| \
+# 参考 v2ray/xray/singbox的路由配置，将geosite是修改配置改一下之后写到对应位置
+awk 1 "$geosite_dir"/category-games@cn \
+      "$geosite_dir"/china-list \
+      "$geosite_dir"/cn \
+      "$geosite_dir"/tld-cn \
+      "$geosite_dir"/win-update \
+      "$geosite_dir"/@cn \
+      "$geosite_dir"/*-cn | \
+awk '!seen[$0]++' >> "$repo_dir"/direct2 #去除重复
+
+# convert_files geosite格式文件 AdGuardHome格式文件 上游dns1 上游dns2 上游dns3 ...
+convert_files "$repo_dir"/private "$repo_dir"/private.txt 192.168.15.1 fd21:bda8:56ba::1
+convert_files "$repo_dir"/direct1 "$repo_dir"/direct1.txt https://dns.alidns.com/dns-query https://doh.pub/dns-query https://doh.360.cn https://doh-pure.onedns.net/dns-query
+convert_files "$repo_dir"/proxy "$repo_dir"/proxy.txt tcp://192.168.15.20:11114 tcp://192.168.15.20:10014 'tcp://[fd21:bda8:56ba:0:222:4dff:fea7:674d]:11116' 'tcp://[fd21:bda8:56ba:0:222:4dff:fea7:674d]:10016'
+convert_files "$repo_dir"/direct2 "$repo_dir"/direct2.txt https://dns.alidns.com/dns-query https://doh.pub/dns-query https://doh.360.cn https://doh-pure.onedns.net/dns-query
+
+# "合并"
+awk 1 "$repo_dir"/private.txt "$repo_dir"/direct1.txt "$repo_dir"/proxy.txt "$repo_dir"/direct2.txt > "$repo_dir"/ADG.txt
+
+# 添加默认上游DNS服务器配置，没有在上述规则以外的其他域名，如：微软，OpenWrt官网等
+cat >> "$repo_dir"/ADG.txt << 'EOF'
+tcp://192.168.15.20:11114
+tcp://192.168.15.20:10014
+tcp://[fd21:bda8:56ba:0:222:4dff:fea7:674d]:11116
+tcp://[fd21:bda8:56ba:0:222:4dff:fea7:674d]:10016
+EOF
 
 # "去重"
 awk -F'[][]' '/^\[\/.*\/\]/ {if (!seen[$2]++) print; next} 1' "$repo_dir"/ADG.txt > "$repo_dir"/ADG_output.txt
@@ -155,18 +233,18 @@ echo "文件 $repo_dir/ADG.txt 有 $(awk 1 "$repo_dir"/ADG.txt | wc -l) 行"
 echo "文件 $target_file/ADG.txt 有 $(awk 1 "$target_file"/ADG.txt | wc -l) 行"
 echo
 
-# 检查命令a是否修改了目录z
-if [ -n "$(git -C $repo_dir status --porcelain)" ]; then
-    echo "检测到变更。正在提交..."
-    git -C "$repo_dir" add .
-    git -C "$repo_dir" commit -S -m "更新 $(date "+%Y-%m-%d %H:%M:%S")"
-    echo "提交完成。"
-    echo "注意输入推送密码..."
-    git -C "$repo_dir" push
-    echo "推送完成。"
-else
-    echo "没有变更。"
-fi
+# 提交更改到仓库
+#if [ -n "$(git -C $repo_dir status --porcelain)" ]; then
+#    echo "检测到变更。正在提交..."
+#    git -C "$repo_dir" add .
+#    git -C "$repo_dir" commit -S -m "更新 $(date "+%Y-%m-%d %H:%M:%S")"
+#    echo "提交完成。"
+#    echo "注意输入推送密码..."
+#    git -C "$repo_dir" push
+#    echo "推送完成。"
+#else
+#    echo "没有变更。"
+#fi
 
 # 目标文件示例
 
